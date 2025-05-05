e70d0f08a2c765d78d332d5bd44a64baf5436e8f
Matthew Eldredge <matthew.eldredge@photon.care>
2025-05-05 09:20:21 -0700

    Remove old favorites.bak pages

--- app/hooks/useSSEEntities.tsx
+++ app/hooks/useSSEEntities.tsx
@@ -3,6 +3,9 @@ import { CharacterType, EpisodeType, LocationType } from "../types";
 
 type EntityType = CharacterType | LocationType | EpisodeType;
 
+// In a more complex application, you might want to use a more sophisticated state management solution (like Redux or Zustand) or even a library like React Query for data fetching and caching.
+// But for this simple example, we'll just use local state and the built-in fetch API.
+// I would separate the the enities each into there own hooks to separate concerns and allow for more unique logic.
 export function useSSEEntities(entityType: "characters" | "locations" | "episodes") {
   const [entities, setEntities] = useState<EntityType[]>([]);
 
