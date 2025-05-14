04d25d34960ca409b56be43f51270ff4454096e8
Matthew Eldredge <matthew.eldredge@photon.care>
2025-05-14 13:03:20 -0700

    unused

--- app/(routes)/episodes/page.tsx
+++ app/(routes)/episodes/page.tsx
@@ -1,9 +1,6 @@
 export const dynamic = "force-dynamic";
-import { listEpisodes } from "actions";
-import { EpisodeType } from "types";
 import EpisodesLandingPage from "../../components/page/EpisodesLandingPage";
 
 export default async function Page() {
-  const episodes: EpisodeType[] = await listEpisodes();
   return <EpisodesLandingPage />;
 }
